34952f4f02952e7fb857801d045cf1dc3f81aae7
suniel <suniel@saediworks.com>
2017-01-09 06:19:29 +0545

    footer controls

--- wp-content/themes/clixel/footer.php
+++ wp-content/themes/clixel/footer.php
@@ -14,12 +14,18 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+		<div class="site-info pull-left">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'clixel' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'clixel' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'clixel' ), 'clixel', '<a href="https://automattic.com/" rel="designer">saediwork</a>' ); ?>
-		</div><!-- .site-info -->
+		</div>
+		<!-- .site-info -->
+		<div class="controls-wrap pull-right">
+			<button id="switch">Switch</button>
+			<div class="controls"></div>
+		</div>
 	</footer><!-- #colophon -->
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
