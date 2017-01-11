8f12c6533e22f9ff954d9155943c237b5c57fa56
subarnaadhikari <adhikari.ritsa@gmail.com>
2017-01-11 12:10:33 +0545

    portfolio and blog

---
+++ wp-content/themes/clixel/template-parts/archive/blog-archive.php
@@ -0,0 +1,63 @@
+<?php /*Template Name: Archive*/?>
+<?php get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php
+		if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+<?php
+ 
+$key_args=array(
+              'post_type' => 'blog',
+              
+              'posts_per_page' => 30,
+              
+              'category_name' => 'blog')
+$query1 = new WP_Query( $key_args );
+ 
+
+while ( $query1->have_posts() ) {
+    $query1->the_post();
+  echo '<li>' . get_the_title() . '</li>';
+  echo '<li>' . get_the_image() . '</li>';
+  echo '<li>' . get_the_category() . '</li>';
+}
+ 
+
+wp_reset_postdata();?>
+		
+			<?php
+			/* Start the Loop */
+			while ( have_posts() ) : the_post();
+
+				/*
+				 * Include the Post-Format-specific template for the content.
+				 * If you want to override this in a child theme, then include a file
+				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				 */
+				get_template_part( 'template-parts/content', get_post_format() );
+
+			endwhile;
+
+			the_posts_navigation();
+
+		else :
+
+			get_template_part( 'template-parts/content', 'none' );
+
+		endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
